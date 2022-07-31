bdfaf7c2892b5a44d047f879e67d223be2a937d6
foxstudent102416 <Ll6vGQPd2ROf>
2022-07-31 12:50:54 +0300

    prop config

---
+++ tests/unittest_api_mock.py
@@ -0,0 +1,26 @@
+import json
+from unittest.mock import Mock
+
+json = Mock()
+json.dumps({'a': 1})
+# json.dumps({'a': 1})
+# print(dir(json))
+# print(json.dumps.call_args)
+# print(json.dumps.call_count)
+# print(json.method_calls)
+# print(json.dumps.called)
+#
+# print(json.dumps.assert_called())
+# print(json.dumps.assert_called_once())
+# print(json.dumps.assert_called_with({'a': 1}))
+
+
+def func(dict):
+    for k, v in dict.items():
+        print(f'print key = {k}, value = {v}')
+
+
+
+json.dumps.side_effect = func
+
+json.dumps({'a': 1})
