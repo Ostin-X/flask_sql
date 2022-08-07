2fd1eceda9096657d5987b1c031555ec2c2d7ca7
foxstudent102416 <Ll6vGQPd2ROf>
2022-08-07 19:49:49 +0300

    some swagger

--- src/course_sql/app.py
+++ src/course_sql/app.py
@@ -1,11 +1,24 @@
 from src.course_sql.extensions.extensions import app
 from src.course_sql.routes.api import api_bp, db
+from flask_swagger_ui import get_swaggerui_blueprint
 
 
 def create_app():
     app.register_blueprint(api_bp)
-    # app.config.from_object('extensions.config.ProdConfig')
     db.init_app(app)
+
+    swagger_url = '/swagger'
+    api_url = '/static/swagger.yaml'
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        swagger_url,
+        api_url,
+        config={
+            'app_name': 'Flask SQL'
+        }
+    )
+
+    app.register_blueprint(swaggerui_blueprint, url_prefix=swagger_url)
+
     return app
 
 
